7e1d9e6ea09367780f41f2306470b2b9b6047db6
Nikita <nikitosik.wolf1@gmail.com>
2025-04-07 00:27:00 +0500

    7.2.cpp

--- Лабораторная работа №7.2/7.2.cpp
+++ Лабораторная работа №7.2/7.2.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
+#include <cstdarg>
 using namespace std;
 
-float mult (int n, float a[])
+float mult (float count, ...)
 {
-    float result = 1;
-    for (int i = 0; i < n; i++)
+    va_list args;
+    va_start(args, count);
+    float result = 1.0;
+    for (int i = 0; i < count; i++)
     {
-        result = result * a[i];
+        float num = (float)va_arg(args, double);
+        result *= num;
     }
+    va_end(args);
     return result;
 }
-
-
 int main ()
 {
-    setlocale(LC_ALL, "Rus");
-    float arr1[] = {3, 2, 4};
-    float arr2[] = {7, 1.5, 2.0, 5.0, 0.5};
-    float arr3[] = {11.0, 2.0, 3.0};
-    cout << "Произведение (3, 2, 4) = " << mult(3, arr1) << endl;
-    cout << "Произведение (7, 1.5, 2, 5, 0.5) = " << mult(5, arr2) << endl;
-    cout << "Произведение (11, 2, 3) = " << mult(3, arr3) << endl;
+    cout << mult(3, 1.0, 2.0, 3.0) << endl;
+    cout << mult(7, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0) << endl;
+    cout << mult(11, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0) << endl;
     return 0;
 }
